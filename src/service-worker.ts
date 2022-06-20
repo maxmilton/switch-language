ad85c643dbeeb1d9673d0c3dcccfb85caff6b24c
Max Milton <max@happycode.ltd>
2022-06-21 06:49:47 +1000

    bug: Don't register content script when not enabled

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -2,7 +2,7 @@
 /// <reference lib="es2020" />
 /// <reference lib="webworker" />
 
-import type { UserSettings } from './components/Switcher';
+import type { UserSettings } from './components/SwitchLanguage';
 
 function handleAppLoad() {
   chrome.storage.local.get(null, (settings: UserSettings) => {
@@ -10,6 +10,27 @@ function handleAppLoad() {
       // console.log('@@@@@ SW globalThis.localStorage', globalThis.localStorage);
 
       // globalThis.localStorage.setItem('__locale__', settings.locale);
+
+      // When the extension is updated the content script is unregistered so we
+      // need to re-register it
+      // @ts-expect-error - missing from @types/chrome, added Chrome v96
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      chrome.scripting.getRegisteredContentScripts((scripts: any[]) => {
+        if (scripts.length === 0) {
+          // @ts-expect-error - missing from @types/chrome, added Chrome v96
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+          chrome.scripting.registerContentScripts([
+            {
+              allFrames: true,
+              id: 'c',
+              js: ['content.js'],
+              matches: ['*://*/*'],
+              runAt: 'document_start',
+              world: 'MAIN',
+            },
+          ]);
+        }
+      });
 
       void chrome.action.setBadgeBackgroundColor({ color: '#0f0' });
       void chrome.action.setBadgeText({ text: settings.locale });
@@ -19,27 +40,6 @@ function handleAppLoad() {
 
 chrome.runtime.onInstalled.addListener(handleAppLoad);
 chrome.runtime.onStartup.addListener(handleAppLoad);
-
-// When the extension is updated the content script is unregistered so we need
-// to re-register it
-// @ts-expect-error - missing from @types/chrome, added Chrome v96
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-chrome.scripting.getRegisteredContentScripts((scripts: any[]) => {
-  if (scripts.length === 0) {
-    // @ts-expect-error - missing from @types/chrome, added Chrome v96
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    chrome.scripting.registerContentScripts([
-      {
-        allFrames: true,
-        id: 'c',
-        js: ['content.js'],
-        matches: ['*://*/*'],
-        runAt: 'document_start',
-        world: 'MAIN',
-      },
-    ]);
-  }
-});
 
 // chrome.runtime.onMessage.addListener((request, sender, reply) => {
 //   console.log('@@@@ SW request', request);
