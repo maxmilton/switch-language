67bc1b57a46e290f22de8dd176d3d16528dced1a
Max Milton <max@happyhackers.xyz>
2024-07-02 14:19:37 +0900

    chore: Tweal eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,7 +31,7 @@ export default tseslint.config(
     },
     languageOptions: {
       parserOptions: {
-        project: ['tsconfig.json'],
+        project: ['tsconfig.json', 'tsconfig.node.json'],
         tsconfigRootDir: import.meta.dirname,
       },
     },
